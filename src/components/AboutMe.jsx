06d07275d39d678a4266b493bdea4fa5715cbf84
peetzaecksdee <peetzaecksdee@hotmail.com>
2024-06-07 20:14:47 +0700

    Add: Image and about me sections

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,13 @@
+export default function AboutMe() {
+  return <>
+    <div className='container'>
+      <span className='nav-title'>
+        ABOUT ME
+      </span>
+      <hr />
+      <span className='text'>
+        yap yap yap
+      </span>
+    </div>
+  </>
+}
---
+++ src/components/ImageContainer.jsx
@@ -0,0 +1,9 @@
+import NAN from '../assets/NAN.png'
+
+export default function Image() {
+	return (
+		<div className="image-container circle">
+			<img src={NAN} />
+		</div>
+	);
+}
