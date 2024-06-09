e8ca0ab4724d538f1ab1d478accdc3e38ae68377
peetzaecksdee <peetzaecksdee@hotmail.com>
2024-06-09 13:58:25 +0700

    Update: Sidebar

--- src/components/ImageContainer.jsx
+++ src/components/ImageContainer.jsx
@@ -1,9 +1,11 @@
-import NAI from '../assets/NAN.png'
+import NAI from '../assets/NAN.png';
 
-export default function Image({ img = NAI}) {
+export default function Image({ img = NAI }) {
 	return (
-		<div className="image-container circle">
-			<img src={img} />
+		<div className="decor">
+			<div className="image-container circle">
+				<img className="profile" src={img} />
+			</div>
 		</div>
 	);
 }
