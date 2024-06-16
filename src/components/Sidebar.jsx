02787e1268dcab4f5d57b81e223bbb0e230fe41f
peetzaecksdee <peetzaecksdee@hotmail.com>
2024-06-16 19:13:09 +0700

    feat: Profile Input

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,12 +2,21 @@ import AboutMe from './Sidebar/AboutMe';
 import Contact from './Sidebar/Contact';
 import Social from './Sidebar/Social';
 import NAI from '../assets/NAN.png';
+import { useState } from 'react';
 
 function Image({ img = NAI }) {
+	const [file, setFile] = useState(img);
+	function handleChange(e) {
+		console.log(e.target.files);
+		setFile(URL.createObjectURL(e.target.files[0]));
+	}
+
 	return (
 		<div className='decor'>
 			<div className='image-container circle'>
-				<input type='image' className='profile' src={img}/>
+				<input type='file' id='file' onChange={handleChange} />
+				<label htmlFor='file' id='fileLabel'></label>
+				<img className='profile' src={file} />
 			</div>
 		</div>
 	);
