72f4cef128970e5ce0d8c9b4ecdc892815d370d4
peetzaecksdee <peetzaecksdee@hotmail.com>
2024-06-17 21:14:26 +0700

    fix: text hidden on overflow + more responsive input

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,23 @@
 import Experience from './Main/Experience';
 import Education from './Main/Education';
 import Skills from './Main/Skills';
+import { useState } from 'react';
 
 Title.defaultProps = {
 	firstName: 'geeza',
-	lastName: 'lala',
+	lastName: 'mama',
 	position: 'freelancer',
 };
 
 function Title({ firstName, lastName, position }) {
+	const [pos, setPos] = useState(position);
 	return (
 		<div className='title-container'>
 			<div className='title'>
 				<span>{firstName.toUpperCase()} </span>
 				<span className='blue'>{lastName.toUpperCase()}</span>
 			</div>
-			<span className='subtitle'>{position.toUpperCase()}</span>
+			<input className='subtitle' value={pos.toUpperCase()} />
 		</div>
 	);
 }
@@ -23,7 +25,7 @@ function Title({ firstName, lastName, position }) {
 export default function Main() {
 	return (
 		<main className='main black'>
-			<Title firstName='geeza' lastName='mama' />
+			<Title />
 			<Experience />
       <Education />
       <Skills />
